643502849ebdda726737abbb9abb6a515c5cd360
shoukugeki02 <59277996+shoukugeki02@users.noreply.github.com>
2020-02-04 17:46:53 +0700

    Update lab11_3.cpp

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,19 +1,27 @@
 #include<iostream>
-
+#include<string>
 using namespace std;
 
-void mySwap (int &,int &); //Pass variables by references
+void mySwap (int &,int &,int&); //Pass variables by references
 
 int main(){
-	int x = 6, y = 9;
+    int x = 6, y = 9, z;
 	cout << "Before swapping:\n";
 	cout << "x = " << x << ", y = " << y << "\n";
-	mySwap (x,y);
+	mySwap (x,y,z);
 	cout << "After swapping:\n";
 	cout << "x = " << x << ", y = " << y << "\n";
 	
 	return 0;
 }
+void mySwap(int &x,int &y,int &z){
+z=x;
+x=y;
+y=z;
+
+}
+
+
 
 //Write function definition here
 /*
